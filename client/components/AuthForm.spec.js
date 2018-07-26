36f96479a4ed860e2e2ddfe10eca85ef2330e0bd
Chris Lusk <chris.m.lusk@gmail.com>
2018-07-26 13:17:34 -0500

    create test based on lecture

---
+++ client/components/AuthForm.spec.js
@@ -0,0 +1,38 @@
+/* global describe beforeEach it */
+/* eslint no-unused-expressions: 0 */
+
+import { expect } from 'chai'
+import React from 'react'
+import enzyme, { shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import sinon from 'sinon'
+import { AuthForm } from './AuthForm'
+
+const adapter = new Adapter()
+enzyme.configure({ adapter })
+
+describe.only('AuthForm', () => {
+  let authForm, submitSpy
+
+  beforeEach(() => {
+    submitSpy = sinon.spy()
+    authForm = shallow(
+      <AuthForm
+        displayName="Login"
+        name="Test Name"
+        handleSubmit={submitSpy}
+      />
+    )
+  })
+
+  it('submits the form with the proper handleSubmit function', () => {
+    const handleSubmit = sinon.spy()
+    console.log('handleSubmit', handleSubmit)
+  })
+
+  it('submits the form with .......? ', () => {
+    const form = authForm.find('form')
+    form.simulate('submit')
+    expect(submitSpy.called).to.be.true
+  })
+})
