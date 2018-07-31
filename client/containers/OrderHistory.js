e547c6c7c2a2a4d49b84c69591d670fabbbf7ad3
jgierlach <jegierlach@gmail.com>
2018-07-31 18:38:23 -0500

    not done yet

--- client/containers/OrderHistory.js
+++ client/containers/OrderHistory.js
@@ -16,15 +16,16 @@ class OrderHistory extends Component {
     return (
       <div>
         <h1 className='title'>Order History</h1>
-          {orderHistory.length && orderHistory.map(order => (
-                    <div key={order[0].id}>
+          {orderHistory.length > 1 && orderHistory.map(order => (
+                    <div key={order.id}>
                     {console.log('order', order)} 
                         <h1>Created at: {order[0].createdAt}</h1>
                          <h1>Order status: {order[0].orderStatusCode.description}</h1>
+                         <h1>Order id: {order[0].id}</h1>
                         <br/>
                     </div>
           ))}
-          {!orderHistory.length && <h1>You have no order history! How do you expect to get drunk?</h1>}
+          {orderHistory.length <= 1 && <h1>You have no order history! How do you expect to reach a state of inebriation?</h1>}
       </div>
     )
   }
